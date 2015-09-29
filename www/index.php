c428404e1090142d76b48fac2456f94a7783a11f
KIRSCH Am�lie <amelie.kirsch@sdw-101-15.local>
2015-09-29 10:02:35 +0200

    changement

---
+++ www/index.php
@@ -0,0 +1,21 @@
+<?php
+  $path ="../..";
+
+  //code...
+
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>TITLE PROJECT</title>
+    <!-- CSS -->
+    <link href="css/styles.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  </head>
+  <body>
+    <?php include "inc/header.inc.php"; ?>
+    <?php include "inc/footer.inc.php"; ?>
+    <!-- Scripts -->
+    <script src="js/jquery.js"></script>
+  </body>
+</html>
